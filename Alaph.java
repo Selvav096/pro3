7b748b5bd1c3171e94e00cf73f5e4f3e506a3ee5
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-27 22:49:48 +0530

    Update Alaph.java

--- Alaph.java
+++ Alaph.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args) 
     {
         Scanner s=new Scanner (System.in);
-        String c=s.next().charAt(0);
+        char c=s.next().charAt(0);
 
         if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
             System.out.println( "Alphabet");
