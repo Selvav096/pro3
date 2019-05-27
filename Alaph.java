d004023d4eff0d1f9c4d01e1841395ed96a8ebee
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-27 22:44:53 +0530

    Create Alaph.java

---
+++ Alaph.java
@@ -0,0 +1,15 @@
+public class Main
+
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner (System.in);
+        String c=s.next().charAt(0);
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println( "Alphabet");
+        else
+            System.out.println("No");
+    }
+}
